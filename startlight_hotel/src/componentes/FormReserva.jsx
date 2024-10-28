59472af208fb50ea3ff28de16a1c05c3817b16fe
Genesis1821 <castillocaprazog@uvm.edu.ve>
2024-10-28 16:44:08 -0400

    formulario reserva

--- startlight_hotel/src/componentes/FormReserva.jsx
+++ startlight_hotel/src/componentes/FormReserva.jsx
@@ -6,7 +6,28 @@ function FormReservas(){
         <div className='contReserva'>        
             <form className="formReserva">
                 <h3 className='tituloForm'>Formulario de reserva</h3>
-                <label htmlFor="fechaIngreso"> Ingresa fecha de llegada <input type="date" id='fechaIngreso' min='28-10-2024' max='28-03-2025'/></label>
+               <label className='labelFecha' > Ingresa fecha de llegada <input type="date" className='fecha' min='28-10-2024' max='28-03-2025'/></label>
+               <label className='labelFecha'> Ingresa fecha de salida <input type="date" className='fecha' min='28-10-2024' max='28-03-2025'/></label>
+               <label className='labelPersona' >
+                <input type="name" placeholder='Ingrese nombre' className='inputs' required  />
+                <input type="email" placeholder='Ingrese correo electronico' className='inputs' required />
+                <input type="number" placeholder='Ingrese cedula' className='inputs' required />
+               </label>
+               <label className='labelPersona' >
+                <input type="name" placeholder='Ingrese nombre' className='inputs'  />
+                <input type="number" placeholder='Ingrese cedula' className='inputs'  />
+               </label>
+               <select name='select' className='inputs'> 
+                <option disabled selected value >Metodo de pago</option>
+                <option value="1">Tarjeta credito</option>
+                <option value="2">Tarjeta debito</option>
+                <option value="3">Trasferencia bancaria</option>
+                <option value="4">Binance</option>
+                <option value="5">Zelle</option>
+                <option value="6">Efectivo</option>
+               </select>
+
+               <button>Enviar Reserva</button>
             </form>
         </div>
 
