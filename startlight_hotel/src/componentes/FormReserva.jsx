63642a00765fd6b72ff1ec3ffb8d14eb9dedffcf
Genesis1821 <castillocaprazog@uvm.edu.ve>
2024-10-29 23:01:25 -0400

    Cambios en formulario

--- startlight_hotel/src/componentes/FormReserva.jsx
+++ startlight_hotel/src/componentes/FormReserva.jsx
@@ -8,16 +8,16 @@ function FormReservas(){
                 <h3 className='tituloForm'>Formulario de reserva</h3>
                <label className='labelFecha' > Ingresa fecha de llegada <input type="date" className='fecha' min='28-10-2024' max='28-03-2025'/></label>
                <label className='labelFecha'> Ingresa fecha de salida <input type="date" className='fecha' min='28-10-2024' max='28-03-2025'/></label>
+               <label ><input type="email" placeholder='Ingrese correo electronico' className='inputCorreo' required /></label>
                <label className='labelPersona' >
                 <input type="name" placeholder='Ingrese nombre' className='inputs' required  />
-                <input type="email" placeholder='Ingrese correo electronico' className='inputs' required />
                 <input type="number" placeholder='Ingrese cedula' className='inputs' required />
                </label>
                <label className='labelPersona' >
                 <input type="name" placeholder='Ingrese nombre' className='inputs'  />
                 <input type="number" placeholder='Ingrese cedula' className='inputs'  />
                </label>
-               <select name='select' className='inputs'> 
+               <select name='select' className='inputs' > 
                 <option disabled selected value >Metodo de pago</option>
                 <option value="1">Tarjeta credito</option>
                 <option value="2">Tarjeta debito</option>
@@ -27,7 +27,7 @@ function FormReservas(){
                 <option value="6">Efectivo</option>
                </select>
 
-               <button>Enviar Reserva</button>
+               <button className='botonEnviar'>Enviar Reserva</button>
             </form>
         </div>
 
